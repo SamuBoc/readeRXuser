7e4591f640bedc6779f1d4dd4a2bbd6217e02321
SamuBoc <samuelandresbonilla@hotmail.com>
2023-05-17 12:00:34 -0500

    BILL CREATE

---
+++ src/model/Bill.java
@@ -0,0 +1,20 @@
+package model;
+
+import java.util.Calendar;
+
+public class Bill {
+    
+    private String id;
+    private Calendar dateSold;
+    private String name;
+
+
+    public Bill(String id, calendar da){
+
+
+        
+    }
+
+
+
+}
--- src/model/Book.java
+++ src/model/Book.java
@@ -1,6 +1,4 @@
 package model;
-
-import java.lang.reflect.Array;
 import java.util.Calendar;
 
 public class Book extends Products{
