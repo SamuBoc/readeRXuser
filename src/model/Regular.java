fc8e94a3f74e0bcc3cfe5bfd8b40461730e257dc
SamuBoc <samuelandresbonilla@hotmail.com>
2023-05-27 19:54:44 -0500

    FILTER_CREATE

--- src/model/Regular.java
+++ src/model/Regular.java
@@ -3,14 +3,14 @@ package model;
 import java.util.ArrayList;
 import java.util.Calendar;
 
-public class Regular extends User{
+public class Regular extends User {
 
     private Book[] books;
     private Magazine[] magazine;
 
     public Regular(String id, String name, String nickname) {
         super(id, name, nickname);
-        //TODO Auto-generated constructor stub
+        // TODO Auto-generated constructor stub
 
         books = new Book[5];
         magazine = new Magazine[2];
@@ -19,30 +19,31 @@ public class Regular extends User{
 
     @Override
     public String toString() {
-        
-        return "Id: " +this.getId() +"\nName: " +this.getName()+ "\nNickname: " + this.getNickname()+ "\nCategory: Regular" ;
+
+        return "Id: " + this.getId() + "\nName: " + this.getName() + "\nNickname: " + this.getNickname()
+                + "\nCategory: Regular";
 
     }
 
     @Override
     public Boolean addBook(Book product) {
-       for(int i = 0; i<books.length; i++){
-        if(books[i] != null){
-            books[i] = product;
-            return true;
+        for (int i = 0; i < books.length; i++) {
+            if (books[i] != null) {
+                books[i] = product;
+                return true;
+            }
         }
-       }
-    return false;
+        return false;
     }
 
     @Override
     public Boolean addMagazine(Magazine product) {
-        for(int i = 0; i<magazine.length; i++){
-            if(magazine[i] != null){
+        for (int i = 0; i < magazine.length; i++) {
+            if (magazine[i] != null) {
                 magazine[i] = product;
                 return true;
             }
-           }
+        }
         return false;
     }
 
@@ -50,24 +51,23 @@ public class Regular extends User{
     public String printMagazine() {
         String msg = "";
 
-        for(int i= 0; i<magazine.length;i++){
-            if(magazine[i] != null){
-                msg += " " +  magazine[i].getIdentifier() + " " + magazine[i].getName() + " \n ";
+        for (int i = 0; i < magazine.length; i++) {
+            if (magazine[i] != null) {
+                msg += " " + magazine[i].getIdentifier() + " " + magazine[i].getName() + " \n ";
             }
         }
-        return msg; 
+        return msg;
     }
 
     @Override
     public boolean magazineDelete(String position) {
-        for(int i = 0; i<magazine.length; i++){
+        for (int i = 0; i < magazine.length; i++) {
             String product = magazine[i].getIdentifier();
-            if(product.equals(position)){
+            if (product.equals(position)) {
                 magazine[i] = null;
                 return true;
             }
         }
-        
 
         return false;
     }
@@ -76,56 +76,58 @@ public class Regular extends User{
     public String viewLibrary() {
         String msg = "";
 
-    String[][] newMatrix = new String[5][5];
+        String[][] newMatrix = new String[5][5];
 
-    ArrayList<Products> products = new ArrayList<Products>();
+        ArrayList<Products> products = new ArrayList<Products>();
 
-    for (int i = 0; i < magazine.length; i++) {
-        if(magazine[i] != null){
-            products.add(magazine[i]);
-        }
-    }
-
-    for (int i = 0; i < books.length; i++) {
-        if(books[i] != null){
-            products.add(books[i]);
-        }
-    }
-
-    for (int i = 0; i < products.size(); i++) {
-        for (int j = i + 1; j < products.size(); j++) {
-            Calendar date1 = products.get(i).getPublicationDate();
-            Calendar date2 = products.get(j).getPublicationDate();
-
-            if (date1.after(date2)) {
-                Products temp = products.get(i);
-                products.set(i, products.get(j));
-                products.set(j, temp);
+        for (int i = 0; i < magazine.length; i++) {
+            if (magazine[i] != null) {
+                Magazine temp = magazine[i];
+                products.add(temp);
             }
         }
-    }
 
-    int index = 0;
-
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
-            if (index < products.size()) {
-                newMatrix[i][j] = products.get(index).getIdentifier();
-                index++;
-            } else {
-                newMatrix[i][j] = "___";
+        for (int i = 0; i < books.length; i++) {
+            if (books[i] != null) {
+                Book temp = books[i];
+                products.add(temp);
             }
         }
-    }
 
-    for (int i = 0; i < newMatrix.length; i++) {
-        for (int j = 0; j < newMatrix[0].length; j++) {
-            msg += newMatrix[i][j] + " ";
+        for (int i = 0; i < products.size(); i++) {
+            for (int j = i + 1; j < products.size(); j++) {
+                Calendar date1 = products.get(i).getPublicationDate();
+                Calendar date2 = products.get(j).getPublicationDate();
+
+                if (date1.after(date2)) {
+                    Products temp = products.get(i);
+                    products.set(i, products.get(j));
+                    products.set(j, temp);
+                }
+            }
         }
-        msg += "\n";
-    }
 
-    return msg;
+        int index = 0;
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (index < products.size()) {
+                    newMatrix[i][j] = products.get(index).getIdentifier();
+                    index++;
+                } else {
+                    newMatrix[i][j] = "___";
+                }
+            }
+        }
+
+        for (int i = 0; i < newMatrix.length; i++) {
+            for (int j = 0; j < newMatrix[0].length; j++) {
+                msg += newMatrix[i][j] + " ";
+            }
+            msg += "\n";
+        }
+
+        return msg;
     }
 
 }
